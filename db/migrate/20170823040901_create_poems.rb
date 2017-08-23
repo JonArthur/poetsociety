66acfe7670705b7b19d5824ba5cc7f037de5b927
JonArthur <jolo.arthur.louize@gmail.com>
2017-08-23 12:09:49 +0800

    Rewritten Poem model

---
+++ db/migrate/20170823040901_create_poems.rb
@@ -0,0 +1,11 @@
+class CreatePoems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :poems do |t|
+      t.string :title
+      t.text :body
+      t.text :author
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170823040901) do
+
+  create_table "poems", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
