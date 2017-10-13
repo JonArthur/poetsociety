4851ad4be07ec0cbd430ed99e90a38d9220e2d49
Jon Arthur Louize Garcia <jolo.arthur.louize@gmail.com>
2017-10-13 17:08:16 +0800

    another fix

--- db/migrate/20170823150859_change_column.rb
+++ db/migrate/20170823150859_change_column.rb
@@ -1,5 +1,5 @@
 class ChangeColumn < ActiveRecord::Migration[5.1]
   def change
-  	change_column :poems, :author,  'integer USING CAST(author AS integer)'
+  	change_column :poems, :author,  author::integer 
   end
 end
